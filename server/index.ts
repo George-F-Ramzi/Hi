432349315a35420a82a14d769e3ba6dfd17f34b3
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-02-26 11:31:48 +0200

    fixing Tailwind Problems

--- server/index.ts
+++ server/index.ts
@@ -8,15 +8,13 @@ import userRoute from "./Routes/userRoute";
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(cors());
 app.use(express.json());
 app.use(userRoute);
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-});
+io.on("connection", (socket) => {});
 
 server.listen(process.env.PORT, () => {
   console.log("Server Started At : " + process.env.PORT);
