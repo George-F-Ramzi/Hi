f501da585b20f79556f5bb4fcb947504fec26e40
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-03-14 14:32:11 +0200

    tryning anthor cors fix

--- client/src/Api/http.ts
+++ client/src/Api/http.ts
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://hi-node-js.vercel.app/",
-  headers: { "x-auth-token": localStorage.getItem("token") },
+  headers: {
+    "x-auth-token": localStorage.getItem("token"),
+    "Access-Control-Allow-Origin": "https://hi-eight-peach.vercel.app/",
+  },
 });
