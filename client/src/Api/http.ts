f28395905f2250bed794b25c66ab04951cc996e6
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-03-14 14:34:55 +0200

    no slash

--- client/src/Api/http.ts
+++ client/src/Api/http.ts
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://hi-node-js.vercel.app/",
   headers: {
     "x-auth-token": localStorage.getItem("token"),
-    "Access-Control-Allow-Origin": "https://hi-eight-peach.vercel.app/",
+    "Access-Control-Allow-Origin": "https://hi-eight-peach.vercel.app",
   },
 });
